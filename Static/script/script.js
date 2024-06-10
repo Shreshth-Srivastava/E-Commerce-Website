12a3e7f825b9324d656994fd433eab50f2e2d136
Shreshth Srivastava <shreshthsrivastava7679@gmail.com>
2024-06-11 01:01:03 +0530

    Added the functionality to view placed orders

--- Static/script/script.js
+++ Static/script/script.js
@@ -19,3 +19,25 @@ category.addEventListener('click',()=>{
         flag = 0;
     }
 })
+
+var Acc = document.querySelector('.Acc');
+var Acc_list = document.querySelector('.Acc_list');
+var acc_arrow = document.querySelector('#acc_arrow');
+var acc_flag = 0;
+Acc.addEventListener('click',()=>{
+    if(!acc_flag){
+        // categoryList.classList.toggle('hidden');
+        Acc_list.style.opacity = '1';
+        Acc_list.style.height = '4rem';
+        acc_arrow.style.transform = 'rotateX(180deg)';
+        // categoryList.style.transform = 'translateY(0)';
+        acc_flag = 1;
+    }
+    else{
+        Acc_list.style.opacity = '0';
+        Acc_list.style.height = '0';
+        acc_arrow.style.transform = 'rotateX(0)';
+        // categoryList.style.transform = 'translateY(-150%)';
+        acc_flag = 0;
+    }
+})
