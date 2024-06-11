2841e32933dc1f82343ae7639096fab2f94444b5
Shreshth Srivastava <shreshthsrivastava7679@gmail.com>
2024-06-11 14:36:58 +0530

    Improved the orders functionality

--- Static/script/wishlist.js
+++ Static/script/wishlist.js
@@ -2,7 +2,8 @@ details = document.querySelectorAll('.Details')
 details.forEach((item) => {
     var userid = item.dataset.userid
     var product_name = item.dataset.name
-    var url = "http://127.0.0.1:8000/product/json/"
+    var root_path = window.location.host
+    var url = `http://${root_path}/product/json/`
     var pk = -1
     fetch(url)
     .then((response) => {
@@ -17,6 +18,6 @@ details.forEach((item) => {
         })
     })
     item.addEventListener('click', () => {
-        window.location.href = `http://127.0.0.1:8000/category/${userid}/${pk}`
+        window.location.href = `http://${root_path}/category/${userid}/${pk}`
     })
 })
