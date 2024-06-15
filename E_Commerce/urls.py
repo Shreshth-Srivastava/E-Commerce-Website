327aba6664741614f3ba9f01f7652cd691c5ec03
Shreshth Srivastava <shreshthsrivastava7679@gmail.com>
2024-06-15 17:59:36 +0530

    Adding Pillow and Hot reloading features

--- E_Commerce/urls.py
+++ E_Commerce/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static 
+from django.conf import settings
 
 urlpatterns = [
     path('', include("App.urls")),
     path('admin/', admin.site.urls),
     path('', include('Authorised_User.urls'), name='login'),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
