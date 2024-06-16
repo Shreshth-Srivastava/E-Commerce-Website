19896afb54694a06d4602d478efdd378d78da661
Shreshth Srivastava <shreshthsrivastava7679@gmail.com>
2024-06-16 22:09:23 +0530

    Updated models

--- App/models.py
+++ App/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Product(models.Model):
-    # value = models.IntegerField(default=1)
     name = models.CharField(default = "Product",max_length=200)
     category = models.CharField(default = 'not defined', max_length = 50)
-    # img = models.CharField(default = "#",max_length=200)
     img = models.ImageField(null=True, blank=True)
     price = models.IntegerField(default = 0)
+    # value = models.IntegerField(default=1)
+    # img = models.CharField(default = "#",max_length=200)
     # value = models.IntegerField(default   = 0)
     # wishlist = models.BooleanField(default = False)
 
@@ -23,7 +23,7 @@ class Customer(models.Model):
     password = models.CharField(default = None, null = True, max_length=15)
     first_name = models.CharField(max_length=20, null=True, blank=True)
     last_name = models.CharField(max_length=20, null=True, blank=True)
-    order_num = models.IntegerField(null=True, blank=True)
+    # order_num = models.IntegerField(null=True, blank=True)
     # userID = models.CharField(default = None, null = True, blank = True, max_length=10)
     # orderPrice = models.IntegerField(default = 0)
     # wishlist = models.IntegerField(default = 0)
@@ -45,10 +45,10 @@ class WishlistItem(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    completed = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=200 ,null=True, blank=True)
     placed = models.BooleanField(default=False)
     payment = models.CharField(max_length=10, blank=True, null=True)
+    # completed = models.BooleanField(default=False)
     # cart = models.IntegerField(default = 0)
     # num_items = models.IntegerField(default=0)
 
