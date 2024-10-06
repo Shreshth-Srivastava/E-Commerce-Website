81998d39d26406a186ab5a11dae034fb0e049653
Shreshth Srivastava <shreshthsrivastava7679@gmail.com>
2024-10-06 17:06:10 +0530

    Optimizing Apps

--- E_Commerce/urls.py
+++ E_Commerce/urls.py
@@ -23,7 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path('', include("App.urls")),
     path('admin/', admin.site.urls),
-    path('', include('Authorised_User.urls'), name='login'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
